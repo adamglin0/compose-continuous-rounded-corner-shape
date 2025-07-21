810e4d3c6528f921bba3e92747ec9500a3289030
adamglin <wwalkingg@gmail.com>
2025-07-21 13:35:35 +0800

    update version to 1.0.2 and update README.md

--- super-rounded-corner-shape/build.gradle.kts
+++ super-rounded-corner-shape/build.gradle.kts
@@ -116,7 +116,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.adamglin",
         artifactId = "compose-continuous-rounded-corner-shape",
-        version = "1.0.1"
+        version = "1.0.2"
     )
     pom {
         name.set("compose-continuous-rounded-corner-shape")
